f2d934e92520d994aa12370986204af7abf0ba96
Denis Lukianov <DenisLukianov2001@yandex.ru>
2023-03-21 20:30:36 +0300

    rework settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -120,11 +120,11 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', default=postgres),
-        'USER': os.getenv('POSTGRES_USER', default=postgres),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=postgres),
-        'HOST': os.getenv('DB_HOST', default=db),
-        'PORT': os.getenv('DB_PORT', default=5432)
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
