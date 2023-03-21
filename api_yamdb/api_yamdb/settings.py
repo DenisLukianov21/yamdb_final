c88a780dbefcb49b5b978c9c50f6add649e84ddc
Denis Lukianov <DenisLukianov2001@yandex.ru>
2023-03-21 20:28:49 +0300

    rework settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ load_dotenv(dotenv_path=env_path)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default=defaultsecretk1y)
 
 DEBUG = False
 
@@ -120,11 +120,11 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'NAME': os.getenv('DB_NAME', default=postgres),
+        'USER': os.getenv('POSTGRES_USER', default=postgres),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=postgres),
+        'HOST': os.getenv('DB_HOST', default=db),
+        'PORT': os.getenv('DB_PORT', default=5432)
     }
 }
 
