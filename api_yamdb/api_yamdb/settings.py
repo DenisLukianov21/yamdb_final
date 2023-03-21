a3a2c6139494c8b28de116bb5ac3a24f1b3f479a
Denis Lukianov <DenisLukianov2001@yandex.ru>
2023-03-21 20:31:36 +0300

    rework settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ load_dotenv(dotenv_path=env_path)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY', default=defaultsecretk1y)
+SECRET_KEY = os.getenv('SECRET_KEY', default='defaultsecretk1y')
 
 DEBUG = False
 
